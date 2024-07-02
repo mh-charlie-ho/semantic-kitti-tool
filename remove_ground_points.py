3cee3b534a15b58133185795e06e52fefd0faa3e
Ho, Ming-Hsin <charlie.ho@itri.org.tw>
2024-07-02 16:19:00 +0800

    update remove ground points

--- remove_ground_points.py
+++ remove_ground_points.py
@@ -1,4 +1,36 @@
 #!/home/charlie/miniconda3/envs/semanticKitti/bin python3
-
-import os
+import load_file as lf
 import numpy as np
+
+ground_point_label = [40]  # the label is only 40.
+
+
+def remove_points(point, label):
+    global ground_point_label
+    checkedList = np.full(len(label), False, dtype=bool)
+    for n in ground_point_label:
+        checkedList = checkedList | (label == n)
+
+    true_indices = np.where(checkedList == 1)
+    return np.delete(point, true_indices,
+                     axis=0), np.delete(label, true_indices)
+
+
+def save_data(pts, path):
+    pts.tofile(path)
+
+
+bin_paths = lf.load_bin_path()
+label_paths = lf.load_label_path()
+for i in range(len(bin_paths)):
+    pt_path = bin_paths[i]
+    label_path = label_paths[i]
+    pt = lf.load_bin(pt_path)
+    label = lf.load_label(label_path)
+    new_pt, new_label = remove_points(pt, label)
+
+    new_pt = new_pt.astype(np.float32)
+    new_label = new_label.astype(np.uint32)
+    save_data(new_pt, pt_path)
+    save_data(new_label, label_path)
+    print(pt_path)

6779c41b38dfdb2965c6e7efe9152e727814f89d
Ho, Ming-Hsin <charlie.ho@itri.org.tw>
2024-07-04 15:09:08 +0800

    remove_ground_points: replace sem label with instance label

--- remove_ground_points.py
+++ remove_ground_points.py
@@ -5,12 +5,13 @@ import numpy as np
 ground_point_label = [40]  # the label is only 40.
 
 
-def remove_points(point, label):
+def remove_points(point, ins_label, sem_label):
     global ground_point_label
-    checkedList = np.full(len(label), False, dtype=bool)
+    checkedList = np.full(len(sem_label), False, dtype=bool)
     for n in ground_point_label:
-        checkedList = checkedList | (label == n)
+        checkedList = checkedList | (sem_label == n)
 
+    label = (ins_label << 16) + ins_label
     true_indices = np.where(checkedList == 1)
     return np.delete(point, true_indices,
                      axis=0), np.delete(label, true_indices)
@@ -26,11 +27,13 @@ for i in range(len(bin_paths)):
     pt_path = bin_paths[i]
     label_path = label_paths[i]
     pt = lf.load_bin(pt_path)
-    label = lf.load_label(label_path)
-    new_pt, new_label = remove_points(pt, label)
+    ins, sem = lf.load_label(label_path, do_inst=True)
+
+    new_pt, new_label = remove_points(pt, ins, sem)
 
     new_pt = new_pt.astype(np.float32)
     new_label = new_label.astype(np.uint32)
+
     save_data(new_pt, pt_path)
     save_data(new_label, label_path)
     print(pt_path)
